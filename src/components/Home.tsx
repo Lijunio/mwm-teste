4884d860e67fb7a929d7d8804302bf0034a42c1a
Elias Junio Souza Ribeiro <eliasjunio.ribeiro95@gmail.com>
2025-02-10 16:44:20 -0300

    alterações

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -49,30 +49,31 @@ const Home = () => {
 
   return (
     <Box sx={{ textAlign: "center", mt: 2, color: "white" }}>
-      {isSmallScreen && (
-        <Box sx={{ mb: 2 }}>
-          <img
-            src={`${process.env.PUBLIC_URL}/imagens/logo.png`}
-            alt="Logo"
-            style={{ width: "250px", height: "auto" }}
-          />
-          <Typography
-            variant="h6"
-            component="p"
-            sx={{
-              mt: 1,
-              fontSize: "1.5rem",
-              fontWeight: "bold",
-              letterSpacing: "3px",
-              textTransform: "uppercase",
-              textAlign: "center",
-              whiteSpace: "nowrap",
-            }}
-          >
-            {visibleText}
-          </Typography>
-        </Box>
-      )}
+      <Box sx={{ mb: 2, textAlign: "center" }}>
+        <img
+          src={`${process.env.PUBLIC_URL}/imagens/logo.png`}
+          alt="Logo"
+          style={{
+            width: isSmallScreen ? "250px" : "300px",
+            height: "auto",
+          }}
+        />
+        <Typography
+          variant="h6"
+          component="p"
+          sx={{
+            mt: 1,
+            fontSize: isSmallScreen ? "1.5rem" : "2rem",
+            fontWeight: "bold",
+            letterSpacing: "3px",
+            textTransform: "uppercase",
+            textAlign: "center",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {visibleText}
+        </Typography>
+      </Box>
 
       <Grid
         container
@@ -83,100 +84,98 @@ const Home = () => {
         }}
       >
         <Grid item xs={12} md={6} order={isSmallScreen ? 1 : 2} sx={{ height: isMediumScreen ? "auto" : "100%" }}>
-        <Card
+          <Card
+            sx={{
+              bgcolor: "transparent",
+              color: "white",
+              textAlign: "center",
+              padding: "16px",
+              height: isSmallScreen ? "auto" : "550px", 
+              width: isSmallScreen ? "100%" : "auto",  
+              border: "none",
+            }}
+          >
+            <CardContent>
+            <Typography
+  variant="h4"
   sx={{
-    bgcolor: "transparent",
+    fontWeight: "bold",
+    fontSize: isSmallScreen ? "1.5rem" : "2rem", 
+    textAlign: "left",
     color: "white",
-    textAlign: "center",
-    padding: "16px",
-    height: isSmallScreen ? "auto" : "550px", 
-    width: isSmallScreen ? "100%" : "auto",  
-    border: "none",
+    marginTop: isSmallScreen ? "0" : "20px",
   }}
 >
-  <CardContent>
+  Há mais de 2 anos no mercado. Na MWM Engenharia, <br />
+  somos especialistas em
+</Typography>
 
-    <Typography
-      variant="h4"
-      sx={{
-        fontWeight: "bold",
-        fontSize: "2rem",
-        textAlign: "left",
-        color: "white",
-        marginTop: isSmallScreen ? "0" : "20px", 
-      }}
-    >
-      Há mais de 2 anos no mercado. Na MWM Engenharia, somos especialistas em
-    </Typography>
-  </CardContent>
+            </CardContent>
 
-  <Typography
-    variant="h6"
-    component="p"
-    sx={{
-      mt: isSmallScreen ? 2 : 4, 
-      fontSize: "1.6rem",
-      textAlign: "left",
-      color: "white",
-    }}
-  >
-    Fabricação, venda e locação mensal de:
-  </Typography>
+            <Typography
+              variant="h6"
+              component="p"
+              sx={{
+                mt: isSmallScreen ? 2 : 4,
+                fontSize: "1.6rem",
+                textAlign: "left",
+                color: "white",
+              }}
+            >
+              Fabricação, venda e locação mensal de:
+            </Typography>
 
-  <Box sx={{ mt: 3, textAlign: "left", maxWidth: "600px", margin: "0 auto" }}>
-    <ul style={{ listStyleType: "none", padding: 0 }}>
-      <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
-        <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
-        Galpão Lonado e com Telhas de Zinco
-      </li>
-      <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
-        <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
-        Galpão Pré-Moldado, Pré-Fabricado e Industrial
-      </li>
-      <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
-        <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
-        Tenda para Obras
-      </li>
-      <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
-        <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
-        Tenda Carpa
-      </li>
-    </ul>
-  </Box>
+            <Box sx={{ mt: 3, textAlign: "left", maxWidth: "600px", margin: "0 auto" }}>
+              <ul style={{ listStyleType: "none", padding: 0 }}>
+                <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
+                  <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
+                  Galpão Lonado e com Telhas de Zinco
+                </li>
+                <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
+                  <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
+                  Galpão Pré-Moldado, Pré-Fabricado e Industrial
+                </li>
+                <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
+                  <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
+                  Tenda para Obras
+                </li>
+                <li style={{ fontSize: "1.4rem", marginBottom: "10px", color: "white" }}>
+                  <FontAwesomeIcon icon={faCaretRight} style={{ marginRight: "10px" }} />
+                  Tenda Carpa
+                </li>
+              </ul>
+            </Box>
 
-  <Button
-  variant="contained"
-  sx={{
-    mt: 3,
-    bgcolor: "#688198",
-    color: "white",
-    width: "100%",
-    fontSize: isSmallScreen ? "1rem" : "1.3rem",
-    padding: isSmallScreen ? "8px" : "12px",
-    background: "linear-gradient(to right,rgb(44, 50, 56),rgb(48, 54, 59))"
-  }}
-  onClick={() => {
-    window.open(
-      "https://wa.me/5531991502088?text=" + encodeURIComponent("Olá! Estive olhando seu site e gostaria de solicitar um orçamento."),
-      "_blank"
-    );
-  }}
->
-  ORÇAMENTO
-</Button>
-
-</Card>
-
+            <Button
+              variant="contained"
+              sx={{
+                mt: 3,
+                bgcolor: "#688198",
+                color: "white",
+                width: "100%",
+                fontSize: isSmallScreen ? "1rem" : "1.3rem",
+                padding: isSmallScreen ? "8px" : "12px",
+                background: "linear-gradient(to right, rgb(44, 50, 56), rgb(48, 54, 59))",
+              }}
+              onClick={() => {
+                window.open(
+                  "https://wa.me/5531991502088?text=" +
+                    encodeURIComponent("Olá! Estive olhando seu site e gostaria de solicitar um orçamento."),
+                  "_blank"
+                );
+              }}
+            >
+              ORÇAMENTO
+            </Button>
+          </Card>
         </Grid>
 
-        <Grid item xs={12} md={6} order={isSmallScreen ? 2 : 1} sx={{ height: isSmallScreen ? "auto" : "100%" }}>
-          <Card sx={{ bgcolor: "transparent", color: "white", textAlign: "center", display: "inline-block", padding: 0 }}>
-            <img
-              src={images[currentImageIndex]}
-              alt={`Image ${currentImageIndex}`}
-              style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "8px" }}
-            />
-          </Card>
+        <Grid item xs={12} md={6} order={isSmallScreen ? 2 : 1}>
+          <img
+            src={images[currentImageIndex]}
+            alt="Imagem de destaque"
+            style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+          />
         </Grid>
       </Grid>
     </Box>
