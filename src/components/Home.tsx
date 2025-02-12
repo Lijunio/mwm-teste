c328f91f009f2bfc340b1b9a05d703fc8ada68b4
Elias Junio Souza Ribeiro <eliasjunio.ribeiro95@gmail.com>
2025-02-12 08:18:18 -0300

    alterações

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -28,137 +28,137 @@ const Home = () => {
 
   return (
     <Box 
+  sx={{
+    height: "80vh",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: isSmallScreen ? "center" : "flex-start",
+    justifyContent: "flex-start",
+    background: `url(${process.env.PUBLIC_URL}/imagens/fundo.png) no-repeat right center`,
+    backgroundSize: "contain",
+    padding: isSmallScreen ? "16px" : "32px",
+    position: "relative",
+    overflow: "hidden", 
+  }}
+>
+  {/* Elemento 2: Logo + Texto */}
+  <Box 
+    sx={{ 
+      textAlign: isSmallScreen ? "center" : "left",
+      zIndex: 2, 
+      width: "100%", 
+      maxWidth: "600px", 
+      marginTop: isSmallScreen ? "-20px" : "20px", // Levantar em telas pequenas
+      marginLeft: isSmallScreen ? "0px" : "250px",
+    }}
+  >
+    <img
+      src={`${process.env.PUBLIC_URL}/imagens/logo.png`}
+      alt="Logo"
+      style={{
+        width: isSmallScreen ? "200px" : "300px",
+        height: "auto",
+        marginBottom: "20px",
+        display: "block",
+        marginLeft: isSmallScreen ? "auto" : "0",
+        marginRight: isSmallScreen ? "auto" : "0",
+      }}
+    />
+    <Typography
+      variant="h6"
+      component="p"
       sx={{
-        height: "80vh",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: isSmallScreen ? "center" : "flex-start", // Centraliza em telas pequenas
-        justifyContent: "flex-start", 
-        background: `url(${process.env.PUBLIC_URL}/imagens/fundo.png) no-repeat right center`,
-        backgroundSize: "contain",
-        padding: isSmallScreen ? "16px" : "32px",
-        position: "relative",
-        overflow: "hidden", 
+        fontSize: isSmallScreen ? "1.2rem" : "2rem",
+        fontWeight: "bold",
+        letterSpacing: "3px",
+        textTransform: "uppercase",
+        textAlign: isSmallScreen ? "center" : "left",
+        color: "white"
       }}
     >
-      {/* Elemento 2: Logo + Texto */}
-      <Box 
-        sx={{ 
-          textAlign: isSmallScreen ? "center" : "left", // Centraliza texto em telas pequenas
-          zIndex: 2, 
-          width: "100%", 
-          maxWidth: "600px", 
-          marginTop: isSmallScreen ? "20px" : "",
-          marginLeft: isSmallScreen ? "0px" : "250px", // Mantém alinhamento à esquerda em telas grandes
-        }}
-      >
-        <img
-          src={`${process.env.PUBLIC_URL}/imagens/logo.png`}
-          alt="Logo"
-          style={{
-            width: isSmallScreen ? "200px" : "300px",
-            height: "auto",
-            marginBottom: "20px",
-            display: "block", // Garante centralização
-            marginLeft: isSmallScreen ? "auto" : "0",
-            marginRight: isSmallScreen ? "auto" : "0",
-          }}
-        />
-        <Typography
-          variant="h6"
-          component="p"
-          sx={{
-            fontSize: isSmallScreen ? "1.2rem" : "2rem",
-            fontWeight: "bold",
-            letterSpacing: "3px",
-            textTransform: "uppercase",
-            textAlign: isSmallScreen ? "center" : "left",
-            color: "white"
-          }}
-        >
-          {visibleText}
-        </Typography>
-      </Box>
+      {visibleText}
+    </Typography>
+  </Box>
 
-      {/* Elemento 3: Card com texto */}
-      <Card
+  {/* Elemento 3: Card com texto */}
+  <Card
+    sx={{
+      bgcolor: "transparent",
+      color: "white",
+      textAlign: isSmallScreen ? "center" : "left",
+      width: "100%",
+      maxWidth: "600px",
+      border: "none",
+      marginTop: isSmallScreen ? "60px" : "40px", // Levantar em telas pequenas
+      marginLeft: isSmallScreen ? "0px" : "200px",
+    }}
+  >
+    <CardContent 
+      sx={{ 
+        backgroundColor: "rgba(37, 75, 58, 0.7)", 
+        borderRadius: "16px", 
+        border: "2px solid rgba(37, 75, 58, 0.5)",
+        padding: isSmallScreen ? "16px" : "32px",
+      }}
+    >
+      <Typography
+        variant="h4"
         sx={{
-          bgcolor: "transparent",
-          color: "white",
-          textAlign: isSmallScreen ? "center" : "left", // Centraliza texto em telas pequenas
-          width: "100%",
-          maxWidth: "600px",
-          border: "none",
-          marginTop: isSmallScreen ? "20px" : "40px",
-          marginLeft: isSmallScreen ? "0px" : "200px", // Mantém alinhamento à esquerda em telas grandes
+          fontWeight: "bold",
+          fontSize: isSmallScreen ? "1.2rem" : "2rem",
+          textAlign: isSmallScreen ? "center" : "left",
+          color: "white", 
+          marginBottom: "20px",  
         }}
       >
-        <CardContent 
-          sx={{ 
-            backgroundColor: "rgba(37, 75, 58, 0.7)", 
-            borderRadius: "16px", 
-            border: "2px solid rgba(37, 75, 58, 0.5)",
-            padding: isSmallScreen ? "16px" : "32px",
-          }}
-        >
-          <Typography
-            variant="h4"
-            sx={{
-              fontWeight: "bold",
-              fontSize: isSmallScreen ? "1.2rem" : "2rem",
-              textAlign: isSmallScreen ? "center" : "left",
-              color: "white", 
-              marginBottom: "20px",  
-            }}
-          >
-            Na MWM Engenharia, somos especialistas em fabricação, venda e locação de:
-          </Typography>
-          <ul style={{ 
-            listStyleType: "disc",  
-            paddingLeft: isSmallScreen ? "0px" : "20px",
-            textAlign: isSmallScreen ? "center" : "left",
-          }}>
-            <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
-              Galpão Lonado e com Telhas de Zinco
-            </li>
-            <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
-              Galpão Pré-Moldado, Pré-Fabricado e Industrial
-            </li>
-            <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
-              Tenda para Obras
-            </li>
-            <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
-              Tenda Carpa
-            </li>
-          </ul>
-        </CardContent>
-      </Card>
+        Na MWM Engenharia, somos especialistas em fabricação, venda e locação de:
+      </Typography>
+      <ul style={{ 
+        listStyleType: "disc",  
+        paddingLeft: isSmallScreen ? "0px" : "20px",
+        textAlign: "left",
+      }}>
+        <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
+          Galpão Lonado e com Telhas de Zinco
+        </li>
+        <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
+          Galpão Pré-Moldado, Pré-Fabricado e Industrial
+        </li>
+        <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
+          Tenda para Obras
+        </li>
+        <li style={{ fontSize: "1.2rem", marginBottom: "10px", color: "white" }}>
+          Tenda Carpa
+        </li>
+      </ul>
+    </CardContent>
+  </Card>
 
-      {/* Elemento 4: Botão abaixo dos elementos 2 e 3 */}
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: isSmallScreen ? "center" : "flex-start", // Centraliza botão apenas em telas pequenas
-          marginTop: isSmallScreen ? "20px" : "40px",
-          marginLeft: isSmallScreen ? "0px" : "200px",
-        }}
-      >
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: "#254B3A",
-            color: "white",
-            padding: "10px 20px",
-            fontSize: "1.2rem",
-            fontWeight: "bold",
-          }}
-        >
-          Solicite seu orçamento
-        </Button>
-      </Box>
-    </Box>
+  {/* Elemento 4: Botão */}
+  <Box
+    sx={{
+      width: "100%",
+      display: "flex",
+      justifyContent: isSmallScreen ? "center" : "flex-start",
+      marginTop: isSmallScreen ? "30px" : "40px", // Levantar botão em telas pequenas
+      marginLeft: isSmallScreen ? "0px" : "200px",
+    }}
+  >
+    <Button
+      variant="contained"
+      sx={{
+        backgroundColor: "#254B3A",
+        color: "white",
+        padding: "10px 20px",
+        fontSize: "1.2rem",
+        fontWeight: "bold",
+      }}
+    >
+      Solicite seu orçamento
+    </Button>
+  </Box>
+</Box>
   );
 };
 
